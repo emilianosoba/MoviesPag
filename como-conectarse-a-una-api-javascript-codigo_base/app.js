0dab3b2596dd47bb5255b9942f6e039e43033df5
emiliano soba <emisoba97@gmail.com>
2023-09-27 19:51:02 -0300

    terminada

--- como-conectarse-a-una-api-javascript-codigo_base/app.js
+++ como-conectarse-a-una-api-javascript-codigo_base/app.js
@@ -1,27 +1,44 @@
+let pagina = 1;
+const btnAnterior = document.getElementById('btnAnterior')
+const btnSiguiente = document.getElementById('btnSiguiente')
 
-const url = 'https://api.themoviedb.org/3/movie/popular?api_key=e4cb3540394cdc895fb0f586f8e22573'
+obtenerPeliculas();
 
-fetch(url)
-.then(response => response.json())
-.then(data => {
-
-    const arrayMovies = data.results;
-    const dataContainer = document.getElementById('contenedor');
-
-    arrayMovies.forEach(pelicula => {
-
-        const div = document.createElement('div');
-        div.classList.add('data-item');
-
-        div.innerHTML = `
-                <h2>${pelicula.title}</h2>
-        `;
-
-        dataContainer.appendChild(div);
-        console.log(data.results)
-    });
-})
-
-.catch(error => {
-    console.error('Error al obtener los datos:', error);
+btnSiguiente.addEventListener('click', () => {
+    pagina += 1;
+    obtenerPeliculas();
 });
+
+function obtenerPeliculas() {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=e4cb3540394cdc895fb0f586f8e22573&language=es-MX&page=${pagina}` // Cambié "lenguage" a "language"
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const arrayMovies = data.results;
+            const dataContainer = document.getElementById('contenedor');
+
+            // Limpia el contenedor antes de agregar nuevas películas
+            dataContainer.innerHTML = '';
+
+            arrayMovies.forEach(pelicula => {
+                const div = document.createElement('div');
+                div.classList.add('data-pelicula');
+
+                div.innerHTML = `
+                    <div class="pelicula">
+                        <img class="poster" src="https://image.tmdb.org/t/p/w500${pelicula.poster_path}">
+                        <h3 class="titulo">${pelicula.title}</h3>
+                    </div>
+                `;
+
+                dataContainer.appendChild(div);
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener los datos:', error);
+        });
+}
+
+
+
